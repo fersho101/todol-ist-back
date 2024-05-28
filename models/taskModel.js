c22edb502d6ebdb570ccb4e0b11547d1bdacd748
fersho101 <corral.fdo@gmail.com>
2024-05-27 17:59:28 -0700

    Se cambio archivos model a export default

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose'
 
-export const taskSchema = mongoose.Schema({
-    text: {
-        type: String,
-        required:[true, 'Please type the task: ']
-    }
-}, {
-    timestamps: true,
-})
+const taskSchema = mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		text: {
+			type: String,
+			required: [true, 'Please type the task: '],
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
 
+export default mongoose.model('Task', taskSchema)
--- models/userModel.js
+++ models/userModel.js
@@ -1,22 +1,27 @@
 import mongoose from 'mongoose'
 
-export const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please type your username: '],
-    },
-    email: {
-        type: String,
-        required: [true, 'Please type your email: '] 
-    },
-    psw: {
-        type: String,
-        required: [true, 'Please, type your password: ']
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    } 
-}, {
-    timestamps: true
-})
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Please type your username: '],
+		},
+		email: {
+			type: String,
+			required: [true, 'Please type your email: '],
+		},
+		psw: {
+			type: String,
+			required: [true, 'Please, type your password: '],
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+export default mongoose.model('User', userSchema)
